5ce472290e71492bea676fe92f3c74ccac00b58b
Ryan OByrne <ryanobyrne2002@gmail.com>
2025-03-13 15:00:04 +0000

    URLs and Views

---
+++ BookaBite/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from BookaBite import views
+
+app_name = 'BookABite'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name="signup"),
+    path('bookings/', views.bookings, name="bookings"),
+    path('bookings/ManageBookings/', views.manageBooking, name='manageBooking'),
+    path('bookings/AddBookings/', views.addBooking, name='addBooking'),
+    path('menu/', views.menu, name='menu'),
+    path('menu/chooseMenu', views.chooseMenu, name="chooseMenu"),
+    path('menu/chooseMenu/showMenu', views.showMenu, name="showMenu"),
+    path('menu/chooseMenu/addMenu', views.addMenu, name="addMenu"),
+    path('menu/chooseMenu/showMenu/addItem', views.addItem, name="addItem"),
+    path('review/', views.review, name="review"),
+    path('review/addReview', views.addReview, name="addReview"),
+    path('review/manageReview', views.manageReview, name="manageReview"),
+]
--- BookaBite/views.py
+++ BookaBite/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Book a Bite Home Page")
+
+def about(request):
+    return HttpResponse("This is the About Page")
+
+def login(request):
+    return HttpResponse("Login Page")
+
+def signup(request):
+    return HttpResponse("Sign Up Page")
+
+def bookings(request):
+    return HttpResponse("Booking Page")
+
+def manageBooking(request):
+    return HttpResponse("Manage Booking Page")
+
+def addBooking(request):
+    return HttpResponse("Add Booking Page")
+
+def menu(request):
+    return HttpResponse("Menu Page")
+
+def chooseMenu(request):
+    return HttpResponse("Choose Menu Page")
+
+def showMenu(request):
+    return HttpResponse("Show Menus Page")
+
+def addMenu(request):
+    return HttpResponse("Add Menu Page")
+
+def addItem(request):
+    return HttpResponse("Add Item Page")
+
+def review(request):
+    return HttpResponse("Review Page")
+
+def addReview(request):
+    return HttpResponse("Add Review Page")
+
+def manageReview(request):
+    return HttpResponse("Manage Review Page")
