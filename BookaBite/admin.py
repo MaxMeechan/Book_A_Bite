0ecaadd06251878abc35b402ce3ca988b6fc15c6
Max Meechan <2894784M@student.gla.ac.uk>
2025-03-14 18:31:27 +0000

    models Built

--- BookaBite/admin.py
+++ BookaBite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from BookaBite.models import Reviews, Bookings, User, Menu, Item
 
 # Register your models here.
+admin.site.register(Reviews)
+admin.site.register(Bookings)
+admin.site.register(User)
+admin.site.register(Menu)
+admin.site.register(Item)
--- BookaBite/models.py
+++ BookaBite/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    Username= models.CharField(max_length=16,unique=True)
+    firstName= models.CharField(max_length=16)
+    surname = models.CharField(max_length=16)
+    email = models.EmailField(unique=True) 
+    Password = models.CharField(max_length=16)
+    profilePicture= models.ImageField(upload_to='profile_images', blank=True)
+    
+
+
+class Bookings(models.Model):
+    bookingId= models.AutoField(primary_key=True)
+    bookingDate= models.DateField()
+    bookingTime= models.TimeField()
+    partyMembers= models.IntegerField()
+    surname= models.CharField(max_length=16)
+    email = models.ForeignKey(User, to_field="email", on_delete=models.CASCADE, related_name="bookings_by_email")
+    
+    class Meta:
+       verbose_name_plural = 'Bookings'
+    
+    def __str__(self):
+      return f"{self.bookingDate} {self.bookingTime}"
+  
+class Reviews(models.Model):
+    ReviewID= models.AutoField(primary_key=True)
+    Title= models.CharField(max_length=30)
+    RatingNum=models.IntegerField()
+    ReviewText= models.CharField(max_length=1000)
+    Username= models.ForeignKey(User, to_field="Username", on_delete=models.CASCADE, related_name="reviews_by_Username")
+    
+    class Meta:
+        verbose_name_plural = 'Reviews'
+    
+    def __str__(self):
+      return f"{self.Title} {self.Username}"
+
+class Menu(models.Model):
+    MenuID= models.AutoField(primary_key=True)
+    MenuName= models.CharField(max_length=30)
+    
+    def __str__(self):
+      return self.MenuName
+    
+class Item(models.Model):
+    ItemID= models.AutoField(primary_key=True)
+    ItemName= models.CharField(max_length=30)
+    ItemPrice= models.DecimalField(max_digits=10, decimal_places=2)
+    ItemDesc= models.CharField(max_length=100)
+    SpiceLevel= models.IntegerField()
+    MenuName= models.ForeignKey(Menu,on_delete=models.CASCADE)
+    
+    def __str__(self):
+      return self.ItemName
+    
+    
+    
+
+
